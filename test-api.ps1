6f052f6a70b3843bbbb5cafabdd62aa16a75ab8b
TemamAb <temamab@github.com>
2026-01-13 11:32:56 -0800

    Commit all changes

--- test-api.ps1
+++
@@ -1,3 +0,0 @@
-$body = Get-Content test-request.json -Raw
-$response = Invoke-RestMethod -Uri 'http://localhost:3001/api/forge-alpha' -Method Post -ContentType 'application/json' -Body $body
-$response | ConvertTo-Json -Depth 10
